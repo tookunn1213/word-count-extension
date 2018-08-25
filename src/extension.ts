de89358ef7944c6756ac3389972bfcda18bab89c
tookunn1213 <youseitookunn@gmail.com>
2018-08-26 02:08:29 +0900

    [modify] update the status bar

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import {window, commands, Disposable, ExtensionContext, StatusBarAlignment, StatusBarItem, TextDocument} from 'vscode';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -9,19 +10,66 @@ export function activate(context: vscode.ExtensionContext) {
 
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "word-count" is now active!');
+    console.log('Congratulations, your extension "WordCount" is now active!');
+
+    // create a new word count
+    let wordCounter = new WordCounter();
 
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
     let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-        // The code you place here will be executed every time your command is executed
-
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
+        wordCounter.updateWordCount();
     });
 
+    // Add to a list of disposables which are disposed when this extension is deactivated.
+    context.subscriptions.push(wordCounter);
     context.subscriptions.push(disposable);
+}
+
+class WordCounter {
+    private _statusBarItem: StatusBarItem = window.createStatusBarItem(StatusBarAlignment.Left);
+
+    public updateWordCount() {
+        // Get the current text editor
+        let editor = window.activeTextEditor;
+        if (!editor) {
+            this._statusBarItem.hide();
+            return;
+        }
+
+        let doc = editor.document;
+
+        // Only update status if an Markdown file
+        if (doc.languageId === "markdown") {
+            let wordCount = this._getWordCount(doc);
+
+            // Update the status bar
+            this._statusBarItem.text = wordCount !== 1 ? `${wordCount} Words` : '1 Word';
+            this._statusBarItem.show();
+        } else {
+            this._statusBarItem.hide();
+        }
+    }
+
+    public _getWordCount(doc: TextDocument): number {
+        let docContent = doc.getText();
+
+        // Parse out unwanted whitespace so the split is accurate
+        docContent = docContent.replace(/(< ([^>]+)<)/g, '').replace(/\s+/g, ' ');
+        docContent = docContent.replace(/^\s\s*/, '').replace(/\s\s*$/, '');
+
+        let wordCount = 0;
+        if (docContent !== "") {
+            wordCount = docContent.split(" ").length;
+        }
+
+        return wordCount;
+    }
+
+    dispose() {
+        this._statusBarItem.dispose();
+    }
 }
 
 // this method is called when your extension is deactivated
